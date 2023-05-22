694f1ab70921ec965b785523f9991e28bd238cc9
devid <nnick4673@gmail.com>
2023-05-22 20:00:17 -0300

    Variáveis

---
+++ variaveis.py
@@ -0,0 +1,28 @@
+# Criando variáveis
+
+idade = 18
+nome = 'Devid'
+
+print(idade)
+print(nome)
+
+'''
+    Tipos de variáveis:
+    int: Números inteiros
+    float: Números reais
+    str: Textos
+    bool: Valores lógicos: True e False
+'''
+
+idade = 18 # Exemplo de inteiro
+nome = 'Devid' # Exemplo de string
+altura = 1.70 # Exemplo de float
+estudando = True # Exemplo de bool
+
+# Salvando dados do input em variáveis
+
+lang = input('Qual linguagem você está estudando? ')
+print('Eu estou estudando', lang)
+
+# Exercício de mostrar todas as variavies em um print
+print(idade, nome, altura, estudando)
