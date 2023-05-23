f52b7037b538711c467ae4acba4cbe7a76c26aa1
devid <nnick4673@gmail.com>
2023-05-23 17:50:42 -0300

    Operadores

---
+++ operadores.py
@@ -0,0 +1,39 @@
+# Operadores aritméticos
+
+'''
+    Adição: +
+    Subtração: -
+    Multiplicação: *
+    Divisão: /
+    Divisão inteira: //
+    Módulo: %
+    Potenciação: **
+'''
+
+n1 = int(input('Digite um número inteiro: '))
+n2 = int(input('Digite outro número inteiro: '))
+
+print(n1 + n2)
+print(n1 - n2)
+print(n1 * n2)
+print(n1 / n2)
+print(n1 // n2)
+print(n1 % n2)
+print(n1 ** n2)
+
+# Operadores lógicos
+
+'''
+    Comparação: >, <, >=, <=, ==, !=
+    conectivos lógicos: and, or, not
+'''
+
+print(n1 > n2)
+print(n1 < n2)
+print(n1 >= n2)
+print(n1 <= n2)
+print(n1 == n2)
+print(n1 != n2)
+print(n1 >= n2 and n1 != n2)
+print(n1 < n2 or n1 == n2)
+print(not(n1 != n2))
